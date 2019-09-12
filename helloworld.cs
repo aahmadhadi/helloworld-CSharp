e2d51b1fa63d936da2746d7942e87a9685405cd1
aahmadhadi <ahmadhadijaelani@gmail.com>
2019-09-12 11:48:46 +0700

    Defining Constants

--- helloworld.cs
+++ helloworld.cs
@@ -1,9 +1,17 @@
 using System;
 
-namespace EscapeChar {
+namespace DeclaringConstants {
    class Program {
       static void Main(string[] args) {
-         Console.WriteLine("Hello\tWorld\n\n");
+         const double pi = 3.14159;   
+            
+         // constant declaration 
+         double r;
+         Console.WriteLine("Enter Radius: ");
+         r = Convert.ToDouble(Console.ReadLine());
+            
+         double areaCircle = pi * r * r;
+         Console.WriteLine("Radius: {0}, Area: {1}", r, areaCircle);
          Console.ReadLine();
       }
    }
